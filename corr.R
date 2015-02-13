31172ec11ac4ef320e735cf3adf6bceec1a7070b
erick <erick@autoweb.com>
2015-02-13 15:56:47 -0600

    Parte 3 Completa

--- corr.R
+++ corr.R
@@ -11,29 +11,31 @@ corr <- function(directory, threshold = 0 ) {
         ## variables) required to compute the correlation between
         ## nitrate and sulfate; the default is 0
         matrixTemp <-  matixNobs[ matixNobs[,"nobs"] > threshold, ]
-
         
+        if(  length( matrixTemp[,"id"] ) > 0){
+	        for ( i in matrixTemp[,"id"] ){
+	        	fileName <- c(directory, archivos[i])
+	        	
 
-        for ( i in matrixTemp[,"id"] ){
-        	fileName <- c(directory, archivos[i])
-        	
+				rawData <- read.csv(paste(fileName, collapse="/"))
 
-			rawData <- read.csv(paste(fileName, collapse="/"))
+				temp1 <- subset(rawData,!is.na(rawData[["sulfate"]]))
+	            temp2 <- subset(temp1,!is.na(temp1[["nitrate"]]))
 
-			temp1 <- subset(rawData,!is.na(rawData[["sulfate"]]))
-            temp2 <- subset(temp1,!is.na(temp1[["nitrate"]]))
+				sulfateVector = temp2[['sulfate']] 
+	            nitrateVector = temp2[['nitrate']] 
 
-			sulfateVector = temp2[['sulfate']] 
-            nitrateVector = temp2[['nitrate']] 
+	            if (!exists("corVectorResult")){
+	            	corVectorResult <- cor(sulfateVector,nitrateVector)
+	            }else{
+	            	corVectorResult <- c( corVectorResult, cor(sulfateVector,nitrateVector))
+	            }	
 
-            if (!exists("corVectorResult")){
-            	corVectorResult <- cor(sulfateVector,nitrateVector)
-            }else{
-            	corVectorResult <- c( corVectorResult, cor(sulfateVector,nitrateVector))
-            }	
+	        }  
+	    }else{
+	    	corVectorResult <- vector(mode="numeric", length=0)
+	    }    
 
-        }  
-
-        print(corVectorResult)
+        corVectorResult
         ## Return a numeric vector of correlations
 }
