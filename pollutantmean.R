1ed806af3c8a83d60ce3ce9b8c56119ced3ae172
erick <erick@autoweb.com>
2015-02-13 12:25:17 -0600

    Carga de archivos en Dataset
    Se cargan todos los archivos que se quieren recorrer en el dataset

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,27 +1,25 @@
 pollutantmean <- function(directory, pollutant, id ) {
+	
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
 
         archivos <- list.files(directory,pattern = "\\.csv$")
         print(archivos)
         
-        for (i in id){
-
+        ## *Recorriendo la lista de id's para formar el data set
+        for (i in id){        	
         	fileName <- c(directory, archivos[i])
         	print(paste(fileName, collapse="/"))
-
-        	rawData = read.csv(paste(fileName, collapse="/"))	
-        	print(rawData)
+        	
+        	if (!exists("df_joined")){
+			   df_joined <- read.csv(paste(fileName, collapse="/"))
+			}else{
+				df_joined <- merge(df_joined, read.csv(paste(fileName, collapse="/")) , all=TRUE)
+			}        	        
         }
 
-
-
-		##multmerge = function(directory){
-		##	filenames=list.files(path=directory, full.names=TRUE)
-		##	datalist = lapply(filenames, function(x){read.csv(file=x,header=T)})
-		##	Reduce(function(x,y) {merge(x,y)}, datalist)
-		##}
-		##print(multmerge[2,])
+        print(df_joined)
+		
         ## 'pollutant' is a character vector of length 1 indicating
         ## the name of the pollutant for which we will calculate the
         ## mean; either "sulfate" or "nitrate".
