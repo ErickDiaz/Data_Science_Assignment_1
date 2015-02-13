795949dadc86967b6e752e4c569a5352aae7cc4e
erick <erick@autoweb.com>
2015-02-13 12:40:11 -0600

    Completa Parte 1
    Se agrego pollutantVectorRaw un vector con la columna de pollutant
    solicitada y se calcula la media

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,14 @@
 pollutantmean <- function(directory, pollutant, id ) {
-	
+
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
 
         archivos <- list.files(directory,pattern = "\\.csv$")
         print(archivos)
         
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+
         ## *Recorriendo la lista de id's para formar el data set
         for (i in id){        	
         	fileName <- c(directory, archivos[i])
@@ -24,11 +27,13 @@ pollutantmean <- function(directory, pollutant, id ) {
         ## the name of the pollutant for which we will calculate the
         ## mean; either "sulfate" or "nitrate".
 
-        ## 'id' is an integer vector indicating the monitor ID numbers
-        ## to be used
+        pollutantVectorRaw = df_joined[[pollutant]] 
+
+        print( pollutantVectorRaw[!is.na(pollutantVectorRaw)] )     
 
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
+		mean( pollutantVectorRaw[!is.na(pollutantVectorRaw)] )        
 
 }
 
