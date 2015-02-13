269f5a96613d8da90a9341d79014442febb129b6
erick <erick@autoweb.com>
2015-02-13 14:45:07 -0600

    (temp) archivos que tiene mas cols que threshold
    threshold

--- corr.R
+++ corr.R
@@ -1,11 +1,21 @@
-corr <- function(directory, threshold = 0) {
+corr <- function(directory, threshold ) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
+        archivos <- list.files(directory,pattern = "\\.csv$")
+        
 
+        matixNobs <- complete("specdata", 1:length(archivos))
+
+        matrixTemp <-  matixNobs[ matixNobs[,"nobs"] > threshold, ]
+        print(matrixTemp)
         ## 'threshold' is a numeric vector of length 1 indicating the
         ## number of completely observed observations (on all
         ## variables) required to compute the correlation between
         ## nitrate and sulfate; the default is 0
+
+        #for (archivo in archivos){
+        #	complete()
+        #}  
 
         ## Return a numeric vector of correlations
 }
